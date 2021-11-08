b609a19dd878e0d84e894bf5c5f3eb3bf83d5dd5
ZakariaZair <zakaria.zair@polymtl.ca>
2021-11-08 14:27:42 -0500

    Update exercice.py

--- exercice.py
+++ exercice.py
@@ -8,11 +8,15 @@ import numpy as np
 
 # TODO: Définissez vos fonctions ici (il en manque quelques unes)
 def linear_values() -> np.ndarray:
-    return np.array([])
+    arr = np.linspace(-1.3, 2.5, 64)
+    return arr
 
 
 def coordinate_conversion(cartesian_coordinates: np.ndarray) -> np.ndarray:
-    return np.array([])
+    for coord in cartesian_coordinates:
+       arr = np.append(arr, [[np.sqrt(coord[0] ** 2 + coord[1] ** 2), np.arctan2(coord[1], coord[0])]], 0)
+    
+    return arr
 
 
 def find_closest_index(values: np.ndarray, number: float) -> int:
@@ -21,5 +25,4 @@ def find_closest_index(values: np.ndarray, number: float) -> int:
 
 if __name__ == '__main__':
     # TODO: Appelez vos fonctions ici
-
-    pass
+    print(linear_values())
